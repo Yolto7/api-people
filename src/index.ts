cc72abae5916c5d6496b6719179906a2749286d9
joaquin <joaquin.torres@ext.konecta-group.com>
2025-02-08 20:45:13 -0500

    .

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,13 @@ const container = loadContainer(),
   middlewares = [container.cradle.contextMiddleware.use(), container.cradle.errorInterceptor.use()],
   controller = container.cradle.peopleController;
 
-export const lambdas = {
-  getById: MiddyMiddleware.use(controller.getById.bind(controller), middlewares),
-  search: MiddyMiddleware.use(controller.search.bind(controller), middlewares),
-  getSwapiAll: MiddyMiddleware.use(controller.getSwapiAll.bind(controller), middlewares),
-  create: MiddyMiddleware.use(controller.create.bind(controller), middlewares),
-};
+export const getById = MiddyMiddleware.use(controller.getById.bind(controller), middlewares);
+
+export const search = MiddyMiddleware.use(controller.search.bind(controller), middlewares);
+
+export const getSwapiAll = MiddyMiddleware.use(
+  controller.getSwapiAll.bind(controller),
+  middlewares
+);
+
+export const create = MiddyMiddleware.use(controller.create.bind(controller), middlewares);
